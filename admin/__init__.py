34803a0e401f7375c2d252581ca74ccd4c22012d
Apolojoytz <Sopanha.kim@student.cadt.edu.kh>
2025-12-17 23:42:47 +0700

    Remove all files from repository

--- admin/__init__.py
+++
@@ -1,9 +0,0 @@
-# Admin package initialization
-from .login import admin_login
-from .create_voting import create_voting_session, view_voting_sessions
-from .result import view_results
-from .blockchain_viewer import view_blockchain
-from .logout import admin_logout
-
-__all__ = ['admin_login', 'create_voting_session', 'view_voting_sessions', 
-           'view_results', 'view_blockchain', 'admin_logout']
--- user/logout.py
+++
@@ -1,7 +0,0 @@
-def logout():
-    """Handle user logout"""
-    print("\n" + "="*40)
-    print("LOGGING OUT...")
-    print("Thank you for using the Voting System!")
-    print("="*40)
-    return True
